743f9a6409a21616a79a3e9bc3fbf0ec32ef953c
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2020-11-24 23:12:10 +0100

    submiting form updates myLibrary array

--- Book.js
+++ Book.js
@@ -1,9 +1,27 @@
 let myLibrary = [];
 
-function Book() {
-
+function Book(author, title, numberOfPages, read) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
+    let form = document.getElementById('add-book-form');
+    let authorInput = document.getElementById('book-author').value;
+    let titleInput = document.getElementById('book-title').value;
+    let numberInput = document.getElementById('book-number').value;
+    let readInput = document.getElementById('book-read').value;
 
+    let newBook = {
+        author: authorInput,
+        title: titleInput,
+        numberOfPages: numberInput,
+        read: readInput
+    }
+    myLibrary.push(newBook);
+    form.reset();
+
+    console.table(myLibrary);
 }
